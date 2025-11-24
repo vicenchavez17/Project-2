6f71d10e111434f52d53454fe70a0053d362b30d
Daniel Morales <daniel.de.morales@icloud.com>
2025-11-24 08:59:54 -0800

    feat: add config file

---
+++ config.js
@@ -0,0 +1,18 @@
+const APPAREL_WHITE_LIST = [
+  'shirt', 'tshirt', 't-shirt', 'blouse', 'top',
+  'pants', 'jeans', 'trousers', 'shorts',
+  'dress', 'skirt',
+  'jacket', 'coat', 'hoodie', 'sweater', 'cardigan',
+  'shoes', 'sneakers', 'boots', 'sandals',
+  'hat', 'cap', 'beanie',
+  'sock', 'socks',
+  'tie', 'scarf',
+  'glove', 'gloves',
+  'suit', 'vest',
+  'underwear', 'bra',
+  'swimsuit', 'bikini'
+];
+
+const CONFIG_PORT = 3000;
+
+export { APPAREL_WHITE_LIST, CONFIG_PORT};
